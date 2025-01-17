d33f4b43ae707b245fad6a6cc62ca9bbc3b8ffae
Marc Diallo <mark.devguru@outlook.com>
2025-01-17 13:59:11 -0600

    gets the date at the very moment

--- script.js
+++ script.js
@@ -287,4 +287,7 @@ btnSort.addEventListener('click', function (e) {
 // console.log(2 ** 53 - 1);
 // console.log(Number.MAX_SAFE_INTEGER);
 
-console.log(2000000000000n + ' were present at the wedding');
+// console.log(2000000000000n + ' were present at the wedding');
+
+const now = new Date();
+console.log(now);
