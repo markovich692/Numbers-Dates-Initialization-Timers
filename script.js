570613d82697129a78f636b9b0ef4ee562a9755a
Marc Diallo <mark.devguru@outlook.com>
2025-01-20 12:50:00 -0600

    adds condition to display dates

--- script.js
+++ script.js
@@ -21,9 +21,9 @@ const account1 = {
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2025-01-11T17:01:17.194Z',
+    '2025-01-18T23:36:17.929Z',
+    '2025-01-19T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -91,14 +91,17 @@ const formatMovementDate = function (date) {
 
   console.log(typeof roundedDaysPassed);
 
+  if (roundedDaysPassed === 0) return 'Today';
+  if (roundedDaysPassed === 1) return 'Yesterday';
+  if (roundedDaysPassed <= 7) return `${roundedDaysPassed} days ago`;
+
   const movementsDate = new Date(date);
   const day = `${movementsDate.getDate()}`.padStart(2, '0');
   const month = `${movementsDate.getMonth() + 1}`.padStart(2, '0');
   const year = movementsDate.getFullYear();
 
-  // const fullDate = `${day}/${month}/${year}`;
-
-  return roundedDaysPassed === 0 ? 'Today' : `${roundedDaysPassed} days ago`;
+  const fullDate = `${day}/${month}/${year}`;
+  return fullDate;
 };
 
 // Functions
